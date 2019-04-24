a67361fb61a34b9d44b9a87f5c5ee223177deddb
Peter Karlsson <peterhkarlsson@gmail.com>
2019-04-24 15:23:09 +0200

    Pxx: show start-end addresses

--- src/pxx.c
+++ src/pxx.c
@@ -4,14 +4,7 @@
 
 #include <stdio.h>
 #include <ctype.h>
-
-// Bytes   Description
-// $00-$06   ASCII string "C64File"
-// $07   Always $00
-// $08-$17   Filename in PETASCII, padded with $00 (not $A0, like a D64)
-// $18   Always $00
-// $19   REL file record size ($00 if not a REL file)
-// $1A-??  Program data
+#include <string.h>
 
 #define SIG_LEN 7
 #define FNAME_LEN 15
@@ -23,20 +16,30 @@ typedef struct
     uint8_t filename[FNAME_LEN];
     uint8_t fill1;
     uint8_t rel_size;
-    uint8_t *data;
+    uint8_t start;
 } PACKED pheader;
 
 void pxx(const uint8_t *buffer, const int size)
 {
-    if (size < (int)sizeof(pheader)) {
+    pheader *p = (pheader*)&buffer[0];
+
+    if (size < (int)sizeof(pheader) ||
+        strncmp(p->signature, "C64File", SIG_LEN) != 0) {
         fprintf(stderr, "Not a valid Pxx file\n");
         return;
     }
 
-    pheader *p = (pheader*)&buffer[0];
-
+    printf("Contents:\n");
     for (int i = 0; i < FNAME_LEN; i++)
         printf("%c", isprint(pet_asc[p->filename[i]]) ?
             pet_asc[p->filename[i]] : ' ');
-    printf("\n");
+
+    uint8_t *data = (uint8_t *)&buffer[sizeof(pheader)];
+    uint16_t startaddr = data[0] + (data[1] << 8);
+
+    printf("   $%04x - $%04lx\n", startaddr,
+        (size - sizeof(pheader)) - startaddr);
+
+    printf("\nListing:\n");
+    basic(data, size - sizeof(pheader));
 }
